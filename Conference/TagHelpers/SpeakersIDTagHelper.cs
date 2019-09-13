de3e7b18db1df883718064b2ba7002864dad4046
MariusSimion79 <mariussmn79@gmail.com>
2019-09-13 14:47:06 +0300

    TagHelper values not transferred to model

---
+++ Conference/TagHelpers/SpeakersIDTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Conference.Service;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Conference.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("speakersId", Attributes = ForAttributeName)]
+
+    public class SpeakersIdTagHelper : TagHelper
+    {
+        private readonly ISpeakerService speakerService;
+        private const string ForAttributeName = "asp-for";
+        public SpeakersIdTagHelper(ISpeakerService speakerService)
+        {
+            this.speakerService = speakerService;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var allSpeakers = speakerService.GetAllSpeakers();
+            output.TagName = "select";
+            output.Attributes.Add("class", "form-control");
+
+            foreach (var speaker in allSpeakers)
+            {
+                TagBuilder myOption = new TagBuilder("option")
+                {
+                    TagRenderMode = TagRenderMode.Normal
+                };
+                myOption.Attributes.Add("value", speaker.Id.ToString());
+                myOption.InnerHtml.Append(speaker.Name);
+                output.Content.AppendHtml(myOption);
+            }
+
+        }
+    }
+}
